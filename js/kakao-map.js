400357e66d253e5f1ac63e0e09ae26ac0f73ef04
rinjyu <rinjyu@naver.com>
2024-02-18 16:56:46 +0900

    refactor: 카카오 지도, 갤러리 수정

--- js/kakao-map.js
+++ js/kakao-map.js
@@ -9,17 +9,21 @@ mapContainer.style.height = "350px";
 mapContainer.style.overflow = "hidden";
 map.relayout();
 
+let zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
 let geocoder = new kakao.maps.services.Geocoder();
-    geocoder.addressSearch("서울 성동구 왕십리광장로 17 민자역사 6층", function(result, status) {
+    geocoder.addressSearch("서울 성동구 왕십리광장로 17 민자역사 6F", function(result, status) {
     if (status === kakao.maps.services.Status.OK) {
-        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        let locPosition = new kakao.maps.LatLng(result[0].y, result[0].x);
         let marker = new kakao.maps.Marker({
-                                map: map, position: coords
+                                map: map,
+                                position: locPosition
                             });
-        let infowindow = new kakao.maps.InfoWindow({
+        let infoWindow = new kakao.maps.InfoWindow({
                                 content: '<div style="width:150px; text-align:center; padding:6px 0;">디노체컨벤션웨딩홀</div>'
                             });
-        infowindow.open(map, marker);
-        map.setCenter(coords);
+        infoWindow.open(map, marker);
+        map.setCenter(locPosition);
     }
 });
